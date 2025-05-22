3018ace78fb16ac776e5de1358979ee696f448c8
ziyad-khafagy <142955501+ziyad-khafagy@users.noreply.github.com>
2025-05-22 18:52:26 +0300

    Add files via upload

---
+++ Include/MCAL/ADC/ADC_Private.h
@@ -0,0 +1,17 @@
+/*
+ * ADC_Private.h
+ *
+ *  Created on: Oct 6, 2023
+ *      Author: user
+ */
+
+#ifndef INCLUDE_MCAL_ADC_ADC_PRIVATE_H_
+#define INCLUDE_MCAL_ADC_ADC_PRIVATE_H_
+#define ADMUX (*(volatile u8 *)0x27)
+#define ADCSRA (*(volatile u8 *)0x26)
+#define ADCH (*(volatile u8 *)0x25)
+#define ADCL (*(volatile u8 *)0x24)
+#define ADCLH (*(volatile u16 *)0x24)
+
+#define ADC_CHANNEL_MASK 0b11100000
+#endif /* INCLUDE_MCAL_ADC_ADC_PRIVATE_H_ */
